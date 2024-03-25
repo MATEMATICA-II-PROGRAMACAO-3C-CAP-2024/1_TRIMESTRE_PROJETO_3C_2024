50db9c66817df5750cc768d7133defb8bb580cda
Maria_Clara_Sampaio <127787008+MariaClaraSampaio25@users.noreply.github.com>
2024-03-25 08:43:46 -0300

    aula 6

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
 contadores[0]
-let tempoAtual = new Date();
-contadores[0].textContent= tempoObjetivo1 - tempoAtual;
+contadores[0].textContent= calculadoraTempo(tempoObjetivo1);
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -16,4 +15,11 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+function calculadoraTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = tempoFinal / 1000;
+    return segundos;
+
 }
