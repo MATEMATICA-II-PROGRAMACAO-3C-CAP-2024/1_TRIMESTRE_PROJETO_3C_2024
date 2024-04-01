46ecd4d38f7c9abf369dd1bf435bdf55a6eb75ae
Maria_Clara_Sampaio <127787008+MariaClaraSampaio25@users.noreply.github.com>
2024-04-01 08:51:55 -0300

    aula7

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date ("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date ("2024-10-05T00:00:00");
+const tempoObjetivo4 = new Date ("2024-10-05T00:00:00");
+const tempos=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 contadores[0]
-contadores[0].textContent= calculadoraTempo(tempoObjetivo1);
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -30,3 +34,12 @@ function calculadoraTempo(tempoObjetivo){
     return dias + " dias "+ horas +" horas " + minutos + " minutos " + segundos + " segundos ";
 
 }
+
+function atualizaCronometro(){
+    for (let i=0; i< contadores.length; i++){
+    contadores[i].textContent= calculadoraTempo(tempos[i]);
+
+}
+}
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
