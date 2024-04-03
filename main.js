925ace247395aa36ce81b10dd92c2f6ef7b34086
Maria_Clara_Sampaio <127787008+MariaClaraSampaio25@users.noreply.github.com>
2024-04-03 08:54:19 -0300

    aula7

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
 const tempoObjetivo2 = new Date ("2024-10-05T00:00:00");
-const tempoObjetivo3 = new Date ("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date ("2023-10-05T00:00:00");
 const tempoObjetivo4 = new Date ("2024-10-05T00:00:00");
 const tempos=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
